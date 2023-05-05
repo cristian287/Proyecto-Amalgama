304b46a40f529a4bcfb4199b8e6a242eb479afe2
cristian287 <cristianalvarado287@gmail.com>
2023-05-05 13:41:58 -0300

    MejorasGenerales

--- js/jsCosecha/modoCosecha.js
+++ js/jsCosecha/modoCosecha.js
@@ -1,11 +1,17 @@
 document.getElementById("placeholderSalirModoCosecha").addEventListener("click",function(e){
     modoCosechaOff()
 })
-
+let tamagochiCapturado = ""
 function modoCosecha(){
     mostrarDesaparecer("modoCosechaHud","aparecer")
+    "boton para salir"
+    if (tamagochiCapturado == ""){
+        return
+    }
+    "botones de partes de tamagochi"
 }
 function modoCosechaOff(){
+    "recrear todos los botones"
     mostrarDesaparecer("modoCosechaHud","desaparecer")
     tamagochiMode()
 }

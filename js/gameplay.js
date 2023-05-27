4529103ebd37a43f9f9bd3cb2f81a53a24614b3d
Ale2301 <alejo.2001@live.com.ar>
2023-05-27 19:09:28 -0300

    creado sistema de cambio de armas

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,6 +1,6 @@
 let player
 const armas = [{ //Armas adquiridas
-    nombre:"primaria",
+    nombre:"pistola",
     balas:10,
     balasMaximas:10,
     recarga:"full",
@@ -30,7 +30,7 @@ function iniciarGameplay(){
         botiquinesDisponibles:2, //Cantidad de botiquines disponibles
       }
     if (player.armas.length === 0){
-        let armaPrincipal = armas.filter(a=>a.nombre === "primaria")
+        let armaPrincipal = armas.find(a=>a.nombre === "primaria")
         player.armas.push(armaPrincipal)
     }
 }
@@ -55,3 +55,36 @@ function recargar(){
 function usarBotiquin(){
     console.log("usando botiquien")
 }
+
+
+var triggerDelay = false;
+var triggerTimeout;
+
+document.addEventListener("wheel", function(e) { //Cambiar arma
+  if (!triggerDelay) {
+    triggerDelay = true;
+    var deltaY = e.deltaY;
+    if (deltaY > 0) {
+      player.equipada++;
+      if (player.equipada > player.armasDisponibles) {
+        player.equipada = 0;
+      }
+    } else if (deltaY < 0) {
+      player.equipada--;
+      if (player.equipada < 0) {
+        player.equipada = player.armas.length - 1;
+      }
+    }
+
+    let armaEquipada = player.armas[player.equipada];
+    console.log(armaEquipada)
+    const armaEquipadaDisplay = document.getElementById("weaponH")
+    armaEquipadaDisplay.textContent = "Arma equipada: " +armaEquipada.nombre;
+    //municionActual.textContent = armaEquipada.balas + "/" + armaEquipada.balasMaximas;
+    triggerTimeout = setTimeout(function() {
+      triggerDelay = false;
+    }, 500);
+  } else {
+    return false;
+  }
+});

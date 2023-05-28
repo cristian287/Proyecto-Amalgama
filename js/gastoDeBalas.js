22fbcf594f384ca084ebb358ec23e06d9c2b9caf
Ale2301 <alejo.2001@live.com.ar>
2023-05-28 16:55:49 -0300

    Creado gasto de balas

---
+++ js/gastoDeBalas.js
@@ -0,0 +1,11 @@
+var recargando = false
+const gameArea = document.getElementById("shootArea")
+gameArea.addEventListener("click",function(e){
+    let armaEquipada = player.armas[player.equipada]
+    if (!recargando){
+      if (armaEquipada.balas>0){
+        armaEquipada.balas--
+      }
+    }
+    actualizarBalas()
+})

ee76d707b55b080ee4e67ff4b9a61d4fceaabb7e
Ale2301 <alejo.2001@live.com.ar>
2023-06-06 19:06:45 -0300

    Solucionados errores de display

--- js/interaccionesEnemigo.js
+++ js/interaccionesEnemigo.js
@@ -4,28 +4,27 @@ function damage(event, index, enemigoActual) { //El enemigo te hittea
         enemigoActual.offsetHeight;
         enemigoActual.style.animation = null;
         player.salud = player.salud - 10;
-        salud.style.width = player.salud + "%";
-        salud.textContent = player.salud + "%";
-    }
-    function enemigoRecibeDaño(event, index, enemy) {
-        if ((player.armas[player.equipada].balas>0) && (!recargando)){
-            enemigosVivos[index].vida--;
-            if (enemigosVivos[index].vida === 0) {
-                console.log("removiendo el enemigo " + index);
-                enemy.remove();
-                let existenEnemigosVivos = enemigosVivos.filter(e=>e.vida>0)
-                console.log(existenEnemigosVivos.length)
-                dropItems()
-                if (existenEnemigosVivos.length === 0){
-                    enemigosVivos = []
-                    indiceEnemigo = -1
-                    if (terminarFase){
-                        botonCambioDeFase.textContent = "¿Listo para la siguiente fase?"
-                    }
+        actualizarValoresPantalla()
+    }    
+}
+function enemigoRecibeDaño(event, index, enemy) {
+    if ((player.armas[player.equipada].balas>0) && (!recargando)){
+        enemigosVivos[index].vida--;
+        if (enemigosVivos[index].vida === 0) {
+            console.log("removiendo el enemigo " + index);
+            enemy.remove();
+            let existenEnemigosVivos = enemigosVivos.filter(e=>e.vida>0)
+            console.log(existenEnemigosVivos.length)
+            dropItems()
+            if (existenEnemigosVivos.length === 0){
+                enemigosVivos = []
+                indiceEnemigo = -1
+                if (terminarFase){
+                    //botonCambioDeFase.textContent = "¿Listo para la siguiente fase?"
                 }
             }
         }
-    }    
+    }
 }
   function dropItems(){
     player.balasDisponibles = player.balasDisponibles + (RNG(15))

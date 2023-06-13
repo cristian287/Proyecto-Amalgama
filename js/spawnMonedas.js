f7179734b7958b66295e56d1ea19270e356245ba
cristian287 <cristianalvarado287@gmail.com>
2023-06-13 15:58:16 -0300

    mejorasSpawnCoins

--- js/spawnMonedas.js
+++ js/spawnMonedas.js
@@ -1,10 +1,10 @@
 const setSpawnCoins = document.getElementById("moneySpawn")
-function crearMoneda(cantidad){ //Queda pendiente esparcirlas. Toy podrido. Maten al que inventó el clientRect
+function crearMoneda(cantidad){
     const limites = setSpawnCoins.getBoundingClientRect();
     var minX = 0
     var minY = 0
-    var maxX = limites.right - 140;
-    var maxY = limites.bottom - 140;
+    var maxX = limites.right - 80;
+    var maxY = limites.bottom - 80;
     var coordenadaX = RNG(maxX - minX) + minX
     var coordenadaY = RNG(maxY - minY) + minY
     let monedas = document.createElement("div")

eddc21f4f9d866ae2de8fbca75f4855ca3c8c09c
Ale2301 <alejo.2001@live.com.ar>
2023-06-21 22:29:09 -0300

    Creado el handler de balas

--- js/cambioDeFase.js
+++ js/cambioDeFase.js
@@ -1,10 +1,14 @@
 const finDeRonda = document.getElementById("finalRonda")
 const textoFinDeRonda = document.getElementById("textoFinalRonda")
 document.getElementById("continuarB").addEventListener("click",function(e){
+    setTimeout(() => {
+        puedeDisparar()
+    }, 900);
     transicionar(finDeRonda,"desaparecer")
     iniciarGeneracionAutomatica()
 })
 function cambioDeFase(){
     transicionar(finDeRonda,"aparecer")
     textoFinDeRonda.textContent = "Completada la ronda " + (rondaActual-1)
+    noPuedeDisparar()
 }

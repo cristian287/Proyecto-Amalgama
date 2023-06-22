1749d824c8ad0d986d20ed9f0a96314a6c51608a
Ale2301 <alejo.2001@live.com.ar>
2023-06-21 22:20:34 -0300

    Creados momentos donde disparar o no

--- js/gastoDeBalas.js
+++ js/gastoDeBalas.js
@@ -1,11 +1,20 @@
 var recargando = false
 const gameArea = document.getElementById("shootArea")
-gameArea.addEventListener("click",function(e){
-    let armaEquipada = player.armas[player.equipada]
+
+function puedeDisparar(){
+  gameArea.addEventListener("click", triggerDisparo);
+}
+
+function noPuedeDisparar(){
+  gameArea.removeEventListener("click", triggerDisparo);
+}
+
+function triggerDisparo(){
+  let armaEquipada = player.armas[player.equipada]
     if (!recargando){
       if (armaEquipada.balas>0){
         armaEquipada.balas--
       }
     }
     actualizarValoresPantalla()
-})
+}
